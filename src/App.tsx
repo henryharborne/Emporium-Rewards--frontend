0f322b591ef254089aeb207f457c7c401adca7e6
Henry Harborne <henryharborne@gmail.com>
2025-05-17 18:48:16 -0400

    Removed unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
-import { useUserStore } from './store/useUserStore';
 
 function App() {
-  const admin = useUserStore((state) => state.admin);
-
   return (
     <BrowserRouter>
-
       <main className="p-4">
         <Routes>
           <Route path="/" element={<Home />} />
